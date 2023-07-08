f50f11723cfa5eb890c697210206aad976247f90
sylwia-laboszczak <sylwiajurczyk91@gmail.com>
2023-07-08 22:38:31 +0200

    updated main

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 
 const response = await axios.get("https://news.ycombinator.com/");
 const html = response.data;
-console.log(html);
 
+// Use Cheerio to parse the HTML
+const $ = cheerio.load(html);
+
+// Select all the elements with the class name "athing"
+const articles = $(".athing");
+
+// Loop through the selected elements
+for (const article of articles) {
+	const text = $(article).text().trim();
+    // Log each article's text content to the console
+    console.log(text);
+}
