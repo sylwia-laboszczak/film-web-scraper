f1982c8ca1ffc40f1eb3be61027f5bfa38b7ec42
sylwia-laboszczak <sylwiajurczyk91@gmail.com>
2023-07-08 23:31:07 +0200

    added fetching title and rate

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-async function extractMovie(streamingPlatformUrl) {
+async function extractMovie(streamingPlatformUrl, platformName) {
   const response = await axios.get(streamingPlatformUrl);
   const html = response.data;
 
@@ -9,18 +9,29 @@ async function extractMovie(streamingPlatformUrl) {
   const $ = cheerio.load(html);
 
   // Select all the elements with the class name "athing"
-  const movies = $(".rankingType__title");
+//   const movies = $(".rankingType__card");
+  const movies = [...$(".rankingType__card")].map((e) => {
+    return {
+      title: $(e).find(".rankingType__title").text().trim(),
+      rating: $(e).find(".rankingType__rate--value").text().trim(),
+      platform: platformName
+    };
+  });
 
   // Loop through the selected elements
   for (let index = 0; index < 10; index++) {
     const movie = movies[index];
-    const text = $(movie).text().trim();
+    const title = movie.title
+    const rating = movie.rating
     // Log each article's text content to the console
-    console.log(index+1  + " " + text);
+    console.log(index+1 +" "+platformName + " " + title + " " + rating);
   }
-
 }
 
-await extractMovie("https://www.filmweb.pl/ranking/vod/netflix/film/");
-// await extractMovie("https://www.filmweb.pl/ranking/vod/hbo_max/film");
-// await extractMovie("https://www.filmweb.pl/ranking/vod/disney/film");
+await extractMovie(
+  "https://www.filmweb.pl/ranking/vod/netflix/film/",
+  "NETFLIX"
+);
+// await extractMovie("https://www.filmweb.pl/ranking/vod/hbo_max/film","HBO");
+// await extractMovie("https://www.filmweb.pl/ranking/vod/disney/film","DISNEY");
+// await extractMovie("https://www.filmweb.pl/ranking/vod/canal_plus_manual/film","CANAL PLUS");
