afe3a24d3e56ded416b41dbfd372122fd78a02a3
sylwia-laboszczak <sylwiajurczyk91@gmail.com>
2023-07-09 19:47:55 +0200

    added  more scenarios to tests

--- main.test.js
+++ main.test.js
@@ -16,7 +16,7 @@ describe("DeduplicatAndSortByRating test", () => {
     // given
     const input = [
       { title: "Nimona", rating: 7.35, platform: "netflix" },
-      { title: "Fenomen", rating: 7.50, platform: "disney" },
+      { title: "Fenomen", rating: 7.5, platform: "disney" },
       { title: "Zaopiekujcie się Mayą", rating: 7.08, platform: "hbo_max" },
     ];
 
@@ -24,12 +24,32 @@ describe("DeduplicatAndSortByRating test", () => {
     var result = deduplicateAndSortByRating(input);
 
     // then
-    expect(result.length).toBe(3)
+    expect(result.length).toBe(3);
     expect(result[0][0]).toBe("Fenomen");
-    expect(result[0][2]).toBe(7.50);
+    expect(result[0][2]).toBe(7.5);
     expect(result[1][0]).toBe("Nimona");
     expect(result[1][2]).toBe(7.35);
     expect(result[2][0]).toBe("Zaopiekujcie się Mayą");
     expect(result[2][2]).toBe(7.08);
+  });
+
+  test("Should return uniqe movies with highest rating", () => {
+    // given
+    const input = [
+      { title: "Nimona", rating: 7.35, platform: "netflix" },
+      { title: "Nimona", rating: 7.5, platform: "disney" },
+      { title: "Nimona", rating: 7.08, platform: "hbo_max" },
+      { title: "Fenomen", rating: 7.22, platform: "hbo_max" },
+    ];
+
+    // when
+    var result = deduplicateAndSortByRating(input);
+
+    // then
+    expect(result.length).toBe(2);
+    expect(result[0][0]).toBe("Nimona");
+    expect(result[0][2]).toBe(7.5);
+    expect(result[1][0]).toBe("Fenomen");
+    expect(result[1][2]).toBe(7.22);
   });
 });
